5142e5277ae88488466d617b82ea2f75fc830973
Heartbroken Warrior <8815572+heartbroken-warrior@user.noreply.gitee.com>
2022-10-09 00:14:44 +0800

    adv

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
     <h1>后台管理系统</h1>
+    <a-button type="primary"> Primary </a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed"> Dashed </a-button>
+    <a-button type="danger"> Danger </a-button>
+    <a-config-provider :auto-insert-space-in-button="false">
+      <a-button type="primary"> 按钮 </a-button>
+    </a-config-provider>
+    <a-button type="primary"> 按钮 </a-button>
+    <a-button type="link"> Link </a-button>
     <router-view />
   </div>
 </template>
-
 <style lang="less">
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
+import Antd from 'ant-design-vue';  // 引入 ant-design-vue
+import 'ant-design-vue/dist/antd.css'; // 引入ant-design-vue 样式
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(Antd); //使用 ant-design-vue
 
 new Vue({
   router,
